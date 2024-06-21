fccd3269649bfca451e422653aa3691abc974bbc
GabrielEduardoSilva <102189527+GabrielEduardoSilva@users.noreply.github.com>
2024-06-20 22:30:10 -0300

    First commit

---
+++ CryptoPaper/CryptoPaper/ServiceManager.swift
@@ -0,0 +1,22 @@
+//
+//  ServiceManager.swift
+//  CryptoPaper
+//
+//  Created by Gabriel Eduardo on 20/06/24.
+//
+
+import Foundation
+
+class ServiceManager {
+    func fetchCoins(completionHandler: @escaping ([Film]) -> Void) {
+      // Configurar a variável lotsOfFilms
+      var lotsOfFilms: [Film]
+      
+      // Chamar a API com algum código
+      
+      // Usar os dados da API, atribuir um valor a lotsOfFilms
+      
+      // Dar ao manipulador de conclusão a variável lotsOfFilms
+      completionHandler(lotsOfFilms)
+    }
+}
