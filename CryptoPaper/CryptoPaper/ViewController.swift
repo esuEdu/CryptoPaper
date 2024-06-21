b63460f3e06d9bc4dd3ce2295e35fd5263f65dbb
GabrielEduardoSilva <102189527+GabrielEduardoSilva@users.noreply.github.com>
2024-06-21 16:53:45 -0300

    Funcionando

--- CryptoPaper/CryptoPaper/ViewController.swift
+++ CryptoPaper/CryptoPaper/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     weak var coordinator: MainCoordinator?
+    
+    private let serviceManager = ServiceManager()
+    
+    private var coins: [Coin] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        fetchCoins()
         
         let button = UIButton(type: .system)
         button.setTitle("Go to Second View", for: .normal)
@@ -30,6 +36,20 @@ class ViewController: UIViewController {
 
     @objc func goToSecondView() {
         coordinator?.goToSecondView()
+    }
+    
+    private func fetchCoins() {
+        serviceManager.fetchCoins { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let coins):
+                    self?.coins = coins
+                    print(coins)
+                case .failure(let error):
+                    print("Failed to fetch coins: \(error)")
+                }
+            }
+        }
     }
 }
 
