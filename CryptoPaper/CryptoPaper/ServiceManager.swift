0262d8347fc483c21cf4b0f8640d185c16be6630
GabrielEduardoSilva <102189527+GabrielEduardoSilva@users.noreply.github.com>
2024-06-24 15:49:36 -0300

    Testes incompletos

--- CryptoPaper/CryptoPaper/ServiceManager.swift
+++ CryptoPaper/CryptoPaper/ServiceManager.swift
@@ -14,6 +14,14 @@ struct Coin: Codable {
 }
 
 class ServiceManager {
+    private var session: URLSession
+    
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    
+    
     func fetchCoins(completion: @escaping (Result<[Coin], Error>) -> Void) {
         // Configurando a URL
         let urlString = "https://api.binance.com/api/v3/ticker/price"
@@ -21,30 +29,37 @@ class ServiceManager {
             completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
             return
         }
-
+        
         // Criando a task para chamada da API
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
                 return
             }
-
+            
             // Salvando os dados. Se não der certo, ele retorna um erro.
             guard let data = data else {
                 completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data"])))
                 return
             }
-
+            
             // Decodificando os dados. Se não der certo, ele retorna um erro.
             do {
+                // Array de moedas
                 let coins = try JSONDecoder().decode([Coin].self, from: data)
-                print(coins)
+                
+                // Printando as moedas para garantir
+                for coin in coins {
+                    print(coin)
+                }
+                
                 completion(.success(coins))
+                
             } catch {
                 completion(.failure(error))
             }
         }
-
+        
         // Inicia a task para chamar a API.
         task.resume()
     }
