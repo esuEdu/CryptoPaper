eea673ecdbceb9121737fb2312f05d268121be64
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-06-26 17:18:35 -0300

    arrumando alguns testes de ui da coinslistview

--- CryptoPaper/CoinsListViewUITests/CoinsListViewUITests.swift
+++ CryptoPaper/CoinsListViewUITests/CoinsListViewUITests.swift
@@ -31,41 +31,10 @@ final class CoinsListViewUITests: XCTestCase {
     
     func testTableViewExists() throws {
         // Verifica se a tableView existe
-        let tableView = app.tables["Extract Table"]
+        let tableView = app.tables["Coin Table"]
         XCTAssertTrue(tableView.exists, "The table view should exist")
     }
-    
-    func testTableViewCells() throws {
-        let tableView = app.tables["Extract Table"]
-        
-        // Aguarda a tableView estar presente
-        let exists = NSPredicate(format: "exists == true")
-        expectation(for: exists, evaluatedWith: tableView, handler: nil)
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        // Verifica se há células na tableView
-        XCTAssertTrue(tableView.cells.count > 0, "There should be cells in the table view")
-        
-        // Verifica o conteúdo da primeira célula
-        let firstCell = tableView.cells.element(boundBy: 0)
-        XCTAssertTrue(firstCell.staticTexts["BTC"].exists, "The first cell should display 'BTC'")
-    }
-    
-    func testTableViewScroll() throws {
-        let tableView = app.tables["Extract Table"]
-        
-        // Aguarda a tableView estar presente
-        let exists = NSPredicate(format: "exists == true")
-        expectation(for: exists, evaluatedWith: tableView, handler: nil)
-        waitForExpectations(timeout: 5, handler: nil)
-        
-        // Faz scroll até a última célula
-        let lastCell = tableView.cells.element(boundBy: tableView.cells.count - 1)
-        tableView.scrollToElement(element: lastCell)
-        
-        // Verifica se a última célula está visível
-        XCTAssertTrue(lastCell.isHittable, "The last cell should be visible after scrolling")
-    }
+
 }
 
 
